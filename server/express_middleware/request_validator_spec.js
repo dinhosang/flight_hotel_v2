b96b4c10f9ddee07317a9f728e84359034dd53d5
douglas sangster <douglas@an-email.home>
2018-07-27 19:14:51 +0100

    renamed route validator to request validator. Renamed some tests to make it clear I am currently only testing GET request for amadeus inspiration searches. Added some pending tests to figure out some edge=cases, and to remind myself to test other GET search requests once they are included

---
+++ server/express_middleware/request_validator_spec.js
@@ -0,0 +1,311 @@
+const assert = require('assert');
+const nock = require('nock');
+const mocksHttp = require('node-mocks-http');
+
+import validatorMiddleware from './request_validator.js';
+import logger from '../helper_tools/async_logger.js';
+import URI_ENUM from '../searches/helper_tools/uri_enum.js';
+
+
+describe('testing request validator middleware for GET search requests', () => {
+
+  let validRequest;
+  let response;
+  let stubMiddleware;
+  let result;
+  let dateCreator;
+  let DATE_ENUM;
+
+  // mocha does top level before, then nested befores
+  // then top level beforeEach and then nested beforeEach
+  // when below was a beforeEach it caused issues of undefined variables
+  before('setting up external objects:', () => {
+
+    DATE_ENUM = {
+      VALID: "today",
+      DEFINITELY_OLD: "two days ago",
+      TOO_FUTURE: "far future"
+    }
+
+    dateCreator = (enumValue) => {
+      let date;
+
+      switch(enumValue) {
+        case DATE_ENUM.VALID:
+          date = new Date();
+          break;
+        case DATE_ENUM.DEFINITELY_OLD:
+          date = new Date();
+          date.setDate(date.getDate() -2);
+          break;
+        case DATE_ENUM.TOO_FUTURE:
+          console.log('find out what this value would be to test');
+          break;
+        default:
+          console.log('Not a key in the DATE_ENUM at the top level before()');
+      }
+
+      if (!date) {
+        return 'use correct DATE_ENUM key'
+      }
+
+      const year = date.getFullYear();
+      let month = date.getMonth() + 1;
+      if (month < 10) {
+        month = `0${month}`;
+      }
+      const dayDate = date.getDate();
+      return `${year}-${month}-${dayDate}`;
+    }
+
+    const dateNow = new Date();
+    const year = dateNow.getFullYear();
+    let month = dateNow.getMonth() + 1;
+    if (month < 10) {
+      month = `0${month}`;
+    }
+    const dayDate = dateNow.getDate();
+    const validIsoDate = `${year}-${month}-${dayDate}`;
+
+    validRequest = () => {
+      return mocksHttp.createRequest({
+        query: {
+          "searchType": "INSPIRATION",
+          "origin": "LON",
+          "departure_date": dateCreator(DATE_ENUM.VALID),
+          "duration": "15"
+        }
+      })
+    };
+
+    response = () => mocksHttp.createResponse();
+
+    stubMiddleware = async () => {
+      result = await Promise.resolve('validated');
+    }
+  });
+
+  context('valid inspiration search GET request treatment:', () => {
+
+    let res;
+
+    before('making valid request:', async () => {
+      result = null;
+      res = response();
+
+      await validatorMiddleware(validRequest(), res, stubMiddleware);
+    })
+
+
+    it('should validate without issue and call next middleware:', async () => {
+      assert.equal(result, 'validated');
+    });
+
+    it('should not send a response:', async () => {
+      assert.equal(res.finished, false);
+    })
+
+  })
+
+  context('response to invalid search GET request:', () => {
+
+    let res;
+
+    before('making invalid request:', async() => {
+      result = null;
+      res = response();
+
+      const invalidRequest = validRequest();
+      invalidRequest.query.duration = 16;
+
+      await validatorMiddleware(invalidRequest,res, stubMiddleware);
+    })
+
+    afterEach('reset result to ensure a clean foundation', () => {
+      result = null;
+    })
+
+    it('returns invalid requests with error object in res body', async () => {
+      result = JSON.parse(res._getData()).error;
+      assert.notStrictEqual(typeof result, 'undefined');
+    })
+
+    it('returns a type key in error object', async () => {
+      result = JSON.parse(res._getData()).error.type;
+      assert.notStrictEqual(typeof result, 'undefined');
+    })
+
+    it('returns a reason key in error object', async () => {
+      result = JSON.parse(res._getData()).error.reason;
+      assert.notStrictEqual(typeof result, 'undefined');
+    })
+
+    it('returns invalid requests with response status code 422', async () => {
+      result = res._getStatusCode();
+      assert.strictEqual(result, 422);
+    })
+  })
+
+  context ('testing forms of invalid search GET request queries:', () => {
+
+    let res;
+    let invalidRequest;
+
+    before('stopping logger from logging each failure', () => {
+      result = null;
+      logger.silent = true;
+    })
+
+    beforeEach('setup for checking types of invalid requests:', () => {
+      res = response();
+      invalidRequest = validRequest();
+    })
+
+    afterEach('resetting result here to protect later contexts', () => {
+      result = null;
+    })
+
+    it('fails any request whose query contains an unexpected key', async () => {
+      invalidRequest.query.aNewKey = 'here is a fake key/value pair';
+
+      await validatorMiddleware(invalidRequest, res, stubMiddleware);
+      assert.notStrictEqual(result, 'validated');
+    })
+
+    context('duration - should fail any request where:', () => {
+
+      it('duration is greater than 15', async () => {
+        invalidRequest.query.duration = 16;
+
+        await validatorMiddleware(invalidRequest, res, stubMiddleware);
+        assert.notStrictEqual(result, 'validated');
+      })
+
+      it('duration equals 0', async () => {
+        invalidRequest.query.duration = 0;
+
+        await validatorMiddleware(invalidRequest, res, stubMiddleware);
+        assert.notStrictEqual(result, 'validated');
+      })
+
+      it('duration is less than 0', async () => {
+        invalidRequest.query.duration = -1;
+
+        await validatorMiddleware(invalidRequest, res, stubMiddleware);
+        assert.notStrictEqual(result, 'validated');
+      })
+
+      it('duration is null', async () => {
+        invalidRequest.query.duration = null;
+
+        await validatorMiddleware(invalidRequest, res, stubMiddleware);
+        assert.notStrictEqual(result, 'validated');
+      })
+
+      it('duration is undefined', async () => {
+        delete invalidRequest.query.duration;
+
+        await validatorMiddleware(invalidRequest, res, stubMiddleware);
+        assert.notStrictEqual(result, 'validated');
+      })
+    })
+
+    context('origin code - should fail any request where:', () => {
+
+      it('origin is not found in amadeus_insp_origins.js when searchType is "INSPIRATION"', async () => {
+        invalidRequest.query.origin = "908";
+
+        await validatorMiddleware(invalidRequest, res, stubMiddleware);
+
+        assert.strictEqual(invalidRequest.query.searchType, "INSPIRATION")
+        assert.notStrictEqual(result, 'validated');
+      })
+
+      it('origin is not a valid IATA code when searchType is not "INSPIRATION", but is one of the other valid types')
+
+      it('origin has a value of null', async () => {
+        invalidRequest.query.origin = null;
+
+        await validatorMiddleware(invalidRequest, res, stubMiddleware);
+        assert.notStrictEqual(result, 'validated');
+      })
+
+      it('origin is undefined', async () => {
+        delete invalidRequest.query.origin;
+
+        await validatorMiddleware(invalidRequest, res, stubMiddleware);
+        assert.notStrictEqual(result, 'validated');
+      })
+    })
+
+    context('searchType - should fail any request where:', () => {
+      it('searchType is not a key in URI_ENUM', async () => {
+        const validKeys = Object.keys(URI_ENUM);
+        const invalidType = "NotASearch"
+        invalidRequest.query.searchType = invalidType;
+
+        await validatorMiddleware(invalidRequest, res, stubMiddleware);
+
+        assert.notStrictEqual(result, 'validated');
+        assert.notStrictEqual(validKeys.includes(invalidType), true);
+      })
+
+      it('searchType has a value of null', async () => {
+        invalidRequest.query.searchType = null;
+
+        await validatorMiddleware(invalidRequest, res, stubMiddleware);
+        assert.notStrictEqual(result, 'validated');
+      })
+
+      it('searchType is undefined', async () => {
+        delete invalidRequest.query.searchType;
+
+        await validatorMiddleware(invalidRequest, res, stubMiddleware);
+        assert.notStrictEqual(result, 'validated');
+      })
+    })
+
+    // not sure how to deal with people making requests from say the west
+    // coast of the USA who may have a date prior to the server's current date.
+    // or if amadues api similarly takes that into account.
+    // could use origin code to determine current date value there?
+    // would amadeus consider that or will it fail anyt such request?
+    // can try testing in the early morning when it would still be the previous
+    // day in the US to see what the result is on the Amadeus side.
+    // would it depend on location of ip address of requester?
+    context('departure_date - should fail any request where:', () => {
+      it('departure_date is two days prior to todays date', async () => {
+        let invReqQuery = invalidRequest.query;
+        invReqQuery.departure_date = dateCreator(DATE_ENUM.DEFINITELY_OLD);
+
+        await validatorMiddleware(invalidRequest, res, stubMiddleware);
+        assert.notStrictEqual(result, 'validated');
+      })
+
+      it('departure_date is a day prior to origins current date')
+
+      it('departure_date is too far into the future')
+
+      it('departure_date is null', async () => {
+        invalidRequest.query.departure_date = null;
+
+        await validatorMiddleware(invalidRequest, res, stubMiddleware);
+        assert.notStrictEqual(result, 'validated');
+      })
+
+      it('departure_date is undefined', async () => {
+        delete invalidRequest.query.departure_date;
+
+        await validatorMiddleware(invalidRequest, res, stubMiddleware);
+        assert.notStrictEqual(result, 'validated');
+      })
+    })
+
+    after('restarting logger once tests are finished', () => {
+      logger.silent = false;
+    })
+
+
+  })
+
+})
