64254a3ccb3272dd99367dd7e9135e2af2bd0fb6
douglas sangster <douglas@an-email.home>
2018-07-27 14:55:34 +0100

    added in valid origin codes for the amadeus inspiration search, last updated a few months back.Modified scheme to treat only those values as valid origin codes as currently only allowing designing for the inspiration search until the front end code for that has been completed

--- server/express_middleware/route_validator.js
+++ server/express_middleware/route_validator.js
@@ -1,13 +1,15 @@
 import Joi from 'joi';
 
 import logger from '../helper_tools/async_logger.js';
+import inspOriginCodes from './amadeus_inspiration_search_origin_codes.js';
 
 
-const validationSchema = Joi.object({
+const inspValidationSchema = Joi.object({
   searchType: Joi.string().valid("INSPIRATION").required(),
   departure_date: Joi.string().length(10).required()
                       .regex([\d]{4}[-][0-1]{1}[\d]{1}[-][0-3]{1}[\d]{1}),
-  origin: Joi.string().length(3).regex([a-zA-Z]{3}).required(),
+  // origin: Joi.string().length(3).regex([a-zA-Z]{3}).required(),
+  origin: Joi.string().valid(inspOriginCodes).required(),
   duration: Joi.number().min(1).max(15).required(),
   direct: Joi.boolean(),
   one-way: Joi.boolean()
@@ -15,14 +17,12 @@ const validationSchema = Joi.object({
 
 const validationMiddleware = async (req, res, next) => {
 
-  // should change to if(searchType equals inspir. ) then use one schema
-  // where the allowed origins are an array of the accepted roigin codes.
-  // with and else which then applies a more general schema of an array of
-  // just all allowed airport and city codes. Can grab those values from
-  // other apis or just from previous site group attempt.
+  // currently only checks for inspiration searches, with an inspiration schema.
+  // when added more searches will need to add a check for search type,
+  // and an update to the schema, or a new one made.
 
   try {
-    await Joi.validate(req.query, validationSchema, {abortEarly: false});
+    await Joi.validate(req.query, inspValidationSchema, {abortEarly: false});
     next();
   } catch (error) {
     res.status(422).send({Error: "Invalid Queries"})
